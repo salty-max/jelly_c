a1483d9d214cf8f1be8327faae2eb6b3a96310ee
salty-max <max@jellycat.fr>
2024-12-20 15:54:27 +0100

    :green_heart: fix CI linux build

--- src/engine/math/util.h
+++ src/engine/math/util.h
@@ -3,9 +3,15 @@
 /**
  * Converts degrees to radians.
  */
-#define DEG_TO_RAD(deg) ((deg) * (M_PI / 180.0f))
+#define DEG_TO_RAD(deg)                                                        \
+  do {                                                                         \
+    (deg) * (M_PI / 180.0f)                                                    \
+  } while (0)
 
 /**
  * Converts radians to degrees.
  */
-#define RAD_TO_DEG(rad) ((rad) * (180.0f / M_PI))
+#define RAD_TO_DEG(rad)                                                        \
+  do {                                                                         \
+    (rad) * (180.0f / M_PI)                                                    \
+  } while (0)
--- src/engine/util.h
+++ src/engine/util.h
@@ -3,9 +3,13 @@
 #include <stdio.h>
 
 #define ERROR_EXIT(...)                                                        \
-  fprintf(stderr, __VA_ARGS__);                                                \
-  exit(1)
+  do {                                                                         \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    exit(1);                                                                   \
+  } while (0)
 
 #define ERROR_RETURN(R, ...)                                                   \
-  fprintf(stderr, __VA_ARGS__);                                                \
-  return R
+  do {                                                                         \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    return R;                                                                  \
+  } while (0)
